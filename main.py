adc95470618a006c333f5c37a1243d21e45678db
Kamil Hamerlak <39203194+kmlZ@users.noreply.github.com>
2020-04-04 20:33:09 +0200

    Added JSON

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import requests
 from bs4 import BeautifulSoup
 import html.parser
 import html5lib
+import json
 
-# SEARCH_ITEM = input("Wpisz nazwę produktu\n")
-SEARCH_ITEM = "asd"
+SEARCH_ITEM = input("Wpisz nazwę produktu\n")
+# SEARCH_ITEM = "asd"
 
 options = Options()
 options.headless = True
@@ -22,7 +23,7 @@ headers = {
    "User-agent": 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.3'
     }
 
-
+products = []
 
 def search():
     close = WebDriverWait(browser,5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[8]/div/div[2]/div/button")))
@@ -32,14 +33,29 @@ def search():
     soup = BeautifulSoup(requests.get(browser.current_url, headers = headers).content, "html5lib")
     soup.findChildren()
     for element in soup.find_all('div', class_="_9c44d_2H7Kt"):
-        name = element.find('h2', class_="_9c44d_LUA1k").findChild().text
-        price_wrapper = element.find('span', class_="_9c44d_1zemI").text       
-        link = element.find('h2', class_="_9c44d_LUA1k").findChild().get("href")
+        name = ''
+        price = ''
+        link = ''
+        try:
+            name = element.find('h2', class_="_9c44d_LUA1k").findChild().text
+            price_wrapper = element.find('span', class_="_9c44d_1zemI").text       
+            link = element.find('h2', class_="_9c44d_LUA1k").findChild().get("href")
+        except:
+            print("Exception")
+        product = Product(name,price_wrapper,link)
+        products.append(product)
         print("Name: " + name)
         print("Price: " + price_wrapper)
         print("Link: " + link)
         print()
-        
+    
+    with open('products.json', 'w', encoding='utf8') as json_file:
+        data = {}
+        data["Products"] = []
+        for prod in products:
+            data["Products"].append(prod.serialize())
+        json.dump(data,json_file,sort_keys=True, indent=4,ensure_ascii=False)
+
     print("Link do strony allegro: "+browser.current_url)
     time.sleep(5)       
     browser.quit()    
--- product.py
+++ product.py
@@ -3,3 +3,16 @@ class Product:
         self.name = name
         self.price = price
         self.link = link
+
+    def serialize(self):
+        return {
+            "name" : self.name,
+            "price" : self.price,
+            "link" : self.link
+        }
+        
+    def from_json(self, json_):
+        self.name = json_["name"]
+        self.price = json_["price"]
+        self.prev_price = json_["prev_price"]
+        self.link = json_["link"]
