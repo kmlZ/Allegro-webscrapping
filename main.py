43bd982f155b235dfaa9c1285299ff976d0f74dc
Kamil Hamerlak <39203194+kmlZ@users.noreply.github.com>
2020-04-05 13:53:09 +0200

    Bot can search for products on x pages

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ from product import Product
 import requests
 from bs4 import BeautifulSoup
 import html.parser
-import html5lib
 import json
 
 SEARCH_ITEM = input("Wpisz nazwę produktu\n")
 # SEARCH_ITEM = "asd"
 JSNON_FILE_NAME = "search_output/"+SEARCH_ITEM.replace(" ","_") + ".json"
+PAGES_TO_SEARCH = input("Wpisz liczbę stron do przeszukania\n")
+print("Pages to search: " + PAGES_TO_SEARCH)
+PAGES_TO_SEARCH = int(PAGES_TO_SEARCH)
+page = PAGES_TO_SEARCH
 print(JSNON_FILE_NAME)
 
 options = Options()
@@ -27,41 +30,59 @@ headers = {
 
 products = []
 
-def search():
-    close = WebDriverWait(browser,5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button._qdoeh:nth-child(1)")))
-    close.click()
-    browser.find_element_by_css_selector("._d25db_3K7x6").send_keys(SEARCH_ITEM)
-    browser.find_element_by_xpath("/html/body/div[2]/div[3]/header/div/div/div/div/form/button").click()
-    soup = BeautifulSoup(requests.get(browser.current_url, headers = headers).content, "html5lib")
-    soup.findChildren()
+
+
+    
+close = WebDriverWait(browser,5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button._qdoeh:nth-child(1)")))
+close.click()
+browser.find_element_by_css_selector("._d25db_3K7x6").send_keys(SEARCH_ITEM)
+browser.find_element_by_xpath("/html/body/div[2]/div[3]/header/div/div/div/div/form/button").click()
+soup = BeautifulSoup(requests.get(browser.current_url, headers = headers).content, "html.parser")
+current_url = browser.current_url
+while True:
+    if page != 0:
+        try:
+            r = requests.get(browser.current_url + "&p=" + str(page), headers=headers)
+            soup = BeautifulSoup(r.content, "html.parser")
+            r_url = r.url
+        except:
+            break
     for element in soup.find_all('div', class_="_9c44d_2H7Kt"):
+        
         name = ''
         price = ''
         link = ''
+        main_link = ''
         try:
             name = element.find('h2', class_="_9c44d_LUA1k").findChild().text
             price_wrapper = element.find('span', class_="_9c44d_1zemI").text       
             link = element.find('h2', class_="_9c44d_LUA1k").findChild().get("href")
+            main_link = r_url
         except:
             print("Exception")
-        product = Product(name,price_wrapper,link)
+        product = Product(name,price_wrapper,link, main_link)
         products.append(product)
-        print("Name: " + name)
-        print("Price: " + price_wrapper)
-        print("Link: " + link)
-        print()
+       # print("Name: " + name)
+       # print("Price: " + price_wrapper)
+       # print("Link: " + link)
+        #print()
+    print("Page: " + str(page))
+    page = page - 1
+    if page == 0:
+        break
+
+    #print("Link do strony allegro: "+r.url)
     
-    with open(JSNON_FILE_NAME, 'w', encoding='utf8') as json_file:
-        data = {}
-        data["Products"] = []
-        for prod in products:
-            data["Products"].append(prod.serialize())
-        json.dump(data,json_file,sort_keys=True, indent=4,ensure_ascii=False)
+        
+run = 0        
+      
+# browser.quit()    
+print("Browser closed!")
 
-    print("Link do strony allegro: "+browser.current_url)
-    time.sleep(5)       
-    browser.quit()    
-    print("Browser closed!")    
-search()
-
+with open(JSNON_FILE_NAME, 'w', encoding='utf8') as json_file:
+    data = {}
+    data["Products"] = []
+    for prod in products:
+        data["Products"].append(prod.serialize())
+    json.dump(data,json_file,sort_keys=True, indent=4,ensure_ascii=False)
 
--- product.py
+++ product.py
@@ -1,14 +1,17 @@
 class Product:
-    def __init__(self, name, price, link):
+    def __init__(self, name, price, link, main_link):
         self.name = name
         self.price = price
         self.link = link
+        self.main_link = main_link
 
     def serialize(self):
         return {
             "name" : self.name,
             "price" : self.price,
-            "link" : self.link
+            "link" : self.link,
+            "main_link" : self.main_link
+
         }
         
     def from_json(self, json_):
